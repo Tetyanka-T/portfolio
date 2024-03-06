d6551abf38e726a20176987a86a6e87ac09ab81d
Tatyana Tupalo <tatyana080489t@gmail.com>
2024-03-06 15:29:18 +0200

    add en version site

---
+++ js/animation.js
@@ -0,0 +1,41 @@
+const animItems = document.querySelectorAll("._anim-items");
+
+if (animItems.length > 0) {
+  window.addEventListener("scroll", animOnScroll);
+  function animOnScroll() {
+    for (let index = 0; index < animItems.length; index++) {
+      const animItem = animItems[index];
+      const animItemHeight = animItem.offsetHight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if (
+        scrollY > animItemOffset - animItemPoint &&
+        scrollY < animItemOffset + animItemHeight
+      ) {
+        animItem.classList.add("_anime");
+      } else {
+        if (!animItem.classList.contains("_anim-items")) {
+          animItem.classList.remove("_anime");
+        }
+      }
+    }
+  }
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+      scrollTop = window.scrollY || document.documentElement.scrollTop;
+    return {
+      top: rect.top + scrollTop,
+      left: rect.left + scrollLeft,
+    };
+  }
+  // setTimeout(() => {
+  animOnScroll();
+  // }, 300);
+}
